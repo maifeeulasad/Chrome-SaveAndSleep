80344949a88a88affbb0c05b04ba9ebd085b9a4d
Maifee Ul Asad <maifeeulasad@gmail.com>
2019-07-09 13:33:40 +0600

    skeleton

---
+++ background.js
@@ -0,0 +1,9 @@
+chrome.commands.onCommand.addListener(function(command) {
+  	if (command == "Save And Sleep") {
+    		console.log("Save And Sleep");
+  	}
+	else if(command=="Restore And Breath"){
+		console.log("Restore And Breath");
+
+	}
+});
