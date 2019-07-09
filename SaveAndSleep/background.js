ade1766ae7f569e4438dc70d9bf0baf6cbe8f09c
Maifee Ul Asad <maifeeulasad@gmail.com>
2019-07-09 14:13:15 +0600

    all tabs are in log

--- SaveAndSleep/background.js
+++ SaveAndSleep/background.js
@@ -1,6 +1,12 @@
 chrome.commands.onCommand.addListener(function(command) {
   	if (command == "Save And Sleep") {
-    		console.log("Save And Sleep");
+chrome.windows.getAll({populate:true},function(windows){
+  windows.forEach(function(window){
+    window.tabs.forEach(function(tab){
+      console.log(tab.url);
+    });
+  });
+});
   	}
 	else if(command=="Restore And Breath"){
 		console.log("Restore And Breath");
